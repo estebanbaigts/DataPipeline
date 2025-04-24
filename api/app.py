a9c53ce2901b081495dc96c9590500c7b6bf3726
alexandredelmotte <110777652+alexandredelmotte@users.noreply.github.com>
2025-04-24 17:03:21 +0200

    interface web

--- api/app.py
+++ api/app.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+import os
 import mlflow.sklearn
 import numpy as np
 
@@ -6,7 +9,13 @@ app = FastAPI()
 
 model = mlflow.sklearn.load_model("/mlflow/artifacts/model")
 
-@app.get("/predict")
+@app.post("/predict")
 def predict(sepal_width: float):
     prediction = model.predict(np.array([[sepal_width]]))[0]
     return {"predicted_sepal_length": prediction}
+
+
+# Serve static HTML file on GET /predict
+@app.get("/predict", response_class=HTMLResponse)
+async def get_predict_page():
+    return FileResponse("index.html")
